c6eb759857d2ad659c0cef3e7fbee33cdf36c9c0
Özer Y <yilmaztekin.ozer@gmail.com>
2020-04-21 19:52:36 +0300

    Update settings.py

--- beyazgezi/settings.py
+++ beyazgezi/settings.py
@@ -121,7 +121,7 @@ DEBUG_TOOLBAR_PANELS = [
    'debug_toolbar.panels.redirects.RedirectsPanel',
 ]
 
-INTERNAL_IPS = ['127.0.0.1', '10.7.154.254']
+INTERNAL_IPS = ['127.0.0.1', '*']
 
 ROOT_URLCONF = PRODUCT+'.urls'
 
